84be555e03ed76bf589eb404336f7ab0ccf051d4
koaning <vincentwarmerdam@gmail.com>
2025-04-03 11:12:53 +0200

    forms

---
+++ examples/forms.py
@@ -0,0 +1,65 @@
+import marimo
+
+__generated_with = "0.12.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    slider = mo.ui.slider(0, 100, 1, label="slider input")
+    text = mo.ui.text(label="text input")
+    text_area = mo.ui.text_area(label="large text input")
+    return slider, text, text_area
+
+
+@app.cell
+def _(mo, slider, text, text_area):
+    mo.vstack([
+        slider, 
+        text,
+        text_area
+    ])
+    return
+
+
+@app.cell
+def _(slider):
+    slider.value
+    return
+
+
+@app.cell
+def _(mo, slider, text, text_area):
+    md = mo.md("""This is a simple form. 
+
+    {slider}
+
+    {text}
+
+    {text_area}
+    """
+    )
+    form = md.batch(slider=slider, text=text, text_area=text_area).form()
+    return form, md
+
+
+@app.cell
+def _(form):
+    form
+    return
+
+
+@app.cell
+def _(form):
+    form.value
+    return
+
+
+if __name__ == "__main__":
+    app.run()
